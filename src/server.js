381cce4e210aa31b721c918c66b5f14f51772423
prabhatvaity <Pvaity@17>
2020-03-01 19:05:32 +0530

    appname changes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/recipe-book'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+  res.sendFile(path.join(__dirname+'/recipe-book/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
