b6cdf2083622fb216c79e561d95bc4e212ec514d
prabhatvaity <Pvaity@17>
2020-03-01 19:45:56 +0530

    path adjustment

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/recipe-book'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/recipe-book/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/recipe-book/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
