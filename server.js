ef222393386eb4b32c5d199202d58df13e6c4512
prabhatvaity <Pvaity@17>
2020-03-01 22:38:42 +0530

    postinstall path changed

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/recipe-book'));
+app.use(express.static(__dirname + '/recipe-book-pv'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/recipe-book/index.html'));
+  res.sendFile(path.join(__dirname+'/recipe-book-pv/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
